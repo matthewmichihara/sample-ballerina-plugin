31a575cf671a0599c12f5ddc3c578f28da6fd7cd
Matthew Michihara <michihara@google.com>
2019-06-18 10:19:14 -0400

    initial commit

---
+++ src/main/java/com/example/ballerina/HelloPreloadingActivity.java
@@ -0,0 +1,16 @@
+package com.example.ballerina;
+
+import com.intellij.openapi.application.PreloadingActivity;
+import com.intellij.openapi.progress.ProgressIndicator;
+import org.jetbrains.annotations.NotNull;
+import org.wso2.lsp4intellij.IntellijLanguageClient;
+import org.wso2.lsp4intellij.client.languageserver.serverdefinition.RawCommandServerDefinition;
+
+public class HelloPreloadingActivity extends PreloadingActivity {
+  @Override
+  public void preload(@NotNull ProgressIndicator indicator) {
+    RawCommandServerDefinition serverDefinition =
+        new RawCommandServerDefinition("txt", new String[] {"./all-caps-server-4", "--stdio"});
+    IntellijLanguageClient.addServerDefinition(serverDefinition);
+  }
+}
